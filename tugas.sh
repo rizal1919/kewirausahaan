298900adbd40b0d7ff7434208b5cc1ecdc7c736c
Rizal Fathurrahman <rizalfathurr1999@gmail.com>
2021-10-23 20:44:46 +0700

    Add files via upload

---
+++ tugas.sh
@@ -0,0 +1,115 @@
+references:
+- https://gobiz.co.id/pusat-pengetahuan/contoh-bisnis-model-canvas-makanan/
+- https://samahitawirotama.com/business-model-canvas-a-starbucks-case/
+- https://www.jurnal.id/id/blog/2018-memahami-tentang-bisnis-model-kanvas/
+
+customer segmentation ini adalah praktik membagi basis pelanggan menjadi kelompok-kelompok individu yang serupa dalam kategori tertentu, seperti usia, jenis kelamin, minat, dan kebiasaan belanja.
+
+	Untuk siapa kita menciptakan nilai?
+	Siapa customer terpenting kita?
+
+	c.segmentation
+	-1 pencari bungkus kado
+	-2 wedding agent atau event organizer  
+
+value proposition adalah kekuatan dan keunggulan yang menjadi pembeda antara usaha Anda dan usaha lain. Pastikan bahwa uang yang dikeluarkan pelanggan Anda sepadan dengan kualitas produk yang Anda berikan.
+
+	v.proportion
+	-1 mendapat desain bungkus kado yang diinginkan
+	-2 pelaksana acara khusus yang membutuhkan bingkisan souvenir dalam jumlah besar dan semacamnya.
+
+	contoh Customer Segment dan Value Proposition
+
+	Pemasan iklan	(cs)
+	Iklan yang menyasar pengguna Google yang luas dan spesifik (vp)
+
+	Pengguna mesin pencarian Google dan tools buatan Google lainnya (cs)
+	Layanan gratis untuk mesin pencarian dan tools Google (vp)
+
+	Content creator sebuah website	(cs)
+	Kemungkinan mendapatkan uang dari konten di website (vp)
+
+Channel atau saluran adalah bagaimana Anda bisa menyampaikan produk Anda sampai hingga kepada konsumen. bagaimana produk dan layanan bisnis Anda dikirimkan ke pasar?
+	
+	channel
+	-1 media sosial (instagram,whatsapp)
+	-3 website
+	-4 conten marketing (blog,reels)
+
+Pengertian revenue stream adalah aliran pemasukan atau pendapatan suatu perusahaan yang diperoleh dari berbagai sumber. Aliran dana yang dimiliki perusahaan mengindikasikan dari mana uang tersebut berasal, seperti penjualan, penjualan aset, investasi, dan sebagainya.
+
+	r.stream
+	-1 biaya patungan 
+	-2 hasil penjualan jasa (pembeli dapat apa saja dengan harga segitu)
+
+Key resource berarti sumber daya yang dibutuhkan bisnis untuk menjalankan bisnis. Hal-hal yang termasuk key resource adalah:
+	
+	fisik
+	-1 kendaraan
+	-2 koneksi internet
+	-3 bahan packaging dan aksesoris yang diperlukan
+	-4 store yang terseba di seluruh kota di Indonesia (optional)
+
+	hak.intelektual
+	-1 brand
+
+	sdm
+	-1 staf pelaksana
+	-2 desainer logo 
+	-3 driver distributor
+	
+	keuangan
+	-1 saldo tunai
+
+customer relationship, ialah Menjaga customer relationship (hubungan pelanggan) yang baik akan menguntungkan bisnis. Cara usaha Anda menjalin hubungan dengan pelanggan akan sangat berdampak pada penjualan dan image bisnis Anda secara keseluruhan.
+
+	c.relationship
+	-1 menyimpan kontak media sosial pelanggan (wa,ig)
+	-2 menyediakan contact person yang aktif 24 jam
+
+
+key activities ialah kegiatan utama operasional perusahaan. Misalnya untuk Starbucks, penggilingan kopi, pembuatan kopi, kegiatan R&D, hingga kegiatan promosi per harinya 
+
+	k.activities
+	-1 pembungkusan barang untuk kado
+	-2 kegiatan R&D 
+	-3 melakukan promosi setiap harinya
+
+Key partnership (mitra utama) adalah daftar perusahaan/pemasok/pihak eksternal lain yang mungkin Anda perlukan untuk menjalankan bisnis dan memberikan nilai kepada pelanggan. Contohnya seperti pemasok bahan baku makanan, penyedia peralatan masak, dan vendor yang memproduksi kemasan makanan.
+
+	k.partnership
+	-1 vendor pemasok bahan pembungkus kado
+	-2 supplier aksesoris tambahan kado
+	-3 distributor umum (jnt,jne,anteraja)
+
+Cost structure, skema finansial yang membiayai operasional bisnis. Di bagian ini, Anda perlu menghitung biaya yang harus dikeluarkan untuk melaksanakan aktivitas bisnis setiap harinya.
+
+	
+	ukuran 10x10x10 | 10x10x5
+
+	4,7 - 4,8RB
+	Terjual selama 5 bulan berdiri
+	harga: 800
+	960/bulan terjual x 12bln = 10k++
+
+	ukuran 10x10x5
+	2,5
+	harga: 800
+	Terjual/Tahun = 2.5k++
+
+	kertas wrapping
+	10rb/11bulan
+	harga: 100
+	909/bulan x 12 bln = 10k++
+
+	potongan pita warna warni
+	10rb terjual dalam 4 tahun
+	harga: 14k
+	2,5rb / tahun x 12bln = 10k++
+
+	market size total = 10k/tahun (TAM: total available market)
+	segmentasi market size = 7.5k/tahun (SAM: serviceable available market)
+	competitor market size = 5k/tahun (SOM: serviceable obtainable market)
+
+	jadi total market brand kita = 5k orang per tahun dan 417 customer per bulan.
+
